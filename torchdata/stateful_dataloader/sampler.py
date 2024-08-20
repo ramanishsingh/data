e161cae3944dffc889797eebfa4c2f4bf492c07c
Ramanish Singh <ramanish@meta.com>
2024-08-20 09:24:27 -0700

    make DisttributedSampler stateful

--- torchdata/stateful_dataloader/sampler.py
+++ torchdata/stateful_dataloader/sampler.py
@@ -8,6 +8,7 @@ from typing import Any, Dict, Iterator, Optional, Sized
 
 import torch.utils.data.sampler
 from torch.utils.data.dataloader import _InfiniteConstantSampler
+from torch.utils.data import Dataset, Sampler
 
 from .stateful import Stateful
 
@@ -125,3 +126,49 @@ class BatchSampler(torch.utils.data.sampler.BatchSampler, Stateful):
                     batch = [0] * self.batch_size
             if idx_in_batch > 0:
                 yield batch[:idx_in_batch]
+
+
+class _StatefulDistributedSamplerIterator(Iterator[int], Stateful):
+
+    def __init__(self, sampler, parent_iterator: Iterator[int]):
+        self.sampler = sampler
+        self.parent_iterator = parent_iterator
+
+    def __next__(self) -> int:
+        if self.sampler.next_yielded is not None:
+            for _ in range(self.sampler.next_yielded):
+                next(self.parent_iterator)
+
+            self.sampler.yielded = self.sampler.next_yielded
+            self.sampler.next_yielded = None
+
+        val = next(self.parent_iterator)
+        self.sampler.yielded += 1
+        return val
+
+
+class StatefulDistributedSampler(torch.utils.data.distributed.DistributedSampler):
+    _YIELDED = "yielded"
+
+    def __init__(
+        self,
+        dataset: Dataset,
+        num_replicas: Optional[int] = None,
+        rank: Optional[int] = None,
+        shuffle: bool = True,
+        seed: int = 0,
+        drop_last: bool = False,
+    ) -> None:
+
+        super().__init__(dataset, num_replicas, rank, shuffle, seed, drop_last)
+        self.yielded = 0
+        self.next_yielded = None
+
+    def __iter__(self):
+        return _StatefulDistributedSamplerIterator(self, super().__iter__())
+
+    def state_dict(self) -> Dict[str, Any]:
+        return {self._YIELDED: self.yielded}
+
+    def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
+        self.next_yielded = state_dict[self._YIELDED]
